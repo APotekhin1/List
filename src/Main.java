9433d6d8caa74e42ec86de40b596635dc03b131e
APotekhin1 <alex01210@gmail.com>
2022-08-29 12:13:42 +0300

    Добавлена продажа по номеру и по названию

--- src/Main.java
+++ src/Main.java
@@ -19,17 +19,50 @@ public class Main {
                     String goods = scanner.nextLine();
                     listGoods.add(goods);
                     System.out.println(goods + " добавлен в корзину");
+                    break;
                 }
                 case ("2"): {
-
+                    System.out.println("Список покупок:");
+                    shopList(listGoods);
+                    break;
                 }
                 case ("3"): {
+                    System.out.println("Список покупок: ");
+                    shopList(listGoods);
+                    System.out.println("Какую покупку хотите удалить? Введите номер ли название: ");
+                    String numberName = scanner.nextLine();
+                    try {
+                        if (Integer.parseInt(numberName) > 0 &&
+                                Integer.parseInt(numberName) < listGoods.size() + 1) {
+                            int n = Integer.parseInt(numberName) - 1;
+                            String delGoods = listGoods.get(n);
+                            listGoods.remove(n);
+                            System.out.println("Покупка " + delGoods + " удалена. \n" +
+                                    " Список покупок: ");
+                            shopList(listGoods);
+                        }
+                    } catch (NumberFormatException e) {
 
+                        listGoods.remove(numberName);
+                        System.out.println("Покупка " + numberName + " удалена. \n" +
+                                " Список покупок: ");
+                        shopList(listGoods);
+                    }
+                    break;
                 }
                 case ("4"): {
-
+                    System.out.println("Пока.");
+                    System.exit(1);
                 }
             }
+        }
+    }
+    public static void shopList(List<String> listGoods) {
+        if (listGoods.isEmpty()) {
+            System.out.println("Список покупок пуст.");
+        }
+        for (int i = 0; i < listGoods.size(); i++) {
+            System.out.println((i + 1) + ". " + listGoods.get(i));
         }
     }
 }
